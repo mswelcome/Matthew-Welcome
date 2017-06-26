97c607e4c6cbd341e273af7942d27826e66b7266
mswelcome <mattwelcome@gmail.com>
2017-06-26 15:36:31 -0400

    Prints my first and last name

---
+++ msw.rb
@@ -0,0 +1,16 @@
+def first(fname)
+	return "My first name is " + fname
+end
+
+def last(lname)
+	return "My last name is " + lname
+end
+
+def whole(first , last)
+	return "My whole name is " + first + last
+end
+puts first("Matthew")
+puts last("Welcome")
+puts whole("Matthew"," Welcome")
+
+
